2da107845302fdc65d00d1ddde4225f34c2a1e8d
somabrata.pramanik@netelixir.com <NetElixir123>
2021-09-02 02:01:20 +0530

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_view():
-    return "Flask-celery task scheduler!"
+    return "Flask-celery task scheduler running.."
 
 if __name__ == "__main__":
     app.run()
